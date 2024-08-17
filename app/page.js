24f3e31bb33f2e14e8d4792abfe60600ab0abb45
enjiwood <87221016+enjiwood@users.noreply.github.com>
2024-08-16 21:39:03 -0700

    Added clerk functionality and main page

--- app/page.js
+++ app/page.js
@@ -1,123 +1,73 @@
 'use client'
-import { Box, CssBaseline, Grid, Paper, ThemeProvider, Typography, Stack, TextField, Button } from '@mui/material'
+import { Box, CssBaseline, Grid, Paper, ThemeProvider, Typography, Stack, TextField, Button, Container, AppBar, Toolbar } from '@mui/material'
+import { getStripe } from '@/utils/get-stripe'
+import Head from 'next/head'
+import { SignedOut, SignedIn, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <Stack 
-      direction="column"
-      width="100vw"
-      height="100vh"
-      backgroundColor='#0F0F0F'
-    >
-      <Box 
-        width="100%"
-        height="8%"
-        backgroundColor="#040404"
+    <Container maxWidth={false} disableGutters>
+      <Head>
+        <title>Flash Trivia</title>
+        <meta name="Description" content="Test your knowledge with flashcards!" />
+      </Head>
+
+      <AppBar position="static" style={{background: '#040404'}}>
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow: 1}}>
+            <Link href="/" passHref>
+              Flash Trivia
+            </Link>
+          </Typography>
+          <SignedOut>
+            <Button sx={{color: '#FFFFFF', backgroundColor: '#0F0F0F'}}>
+                <Link href="/sign-in" passHref>
+                    Login
+                </Link>
+            </Button>
+            <Button sx={{color: 'orange', backgroundColor: '#0F0F0F'}}>
+                <Link href="/sign-up" passHref>
+                    Sign Up
+                </Link>
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+
+      <Stack 
+        direction="column"
+        width="100vw"
+        height="100vh"
+        backgroundColor='#0F0F0F'
+        justifyContent="center"
       >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
+        <Box
+          width="100%"
+          display="flex"
+          flexDirection="column"
           alignItems="center"
-          p={1}
-          pl={3}
-          pr={3}
         >
-          <Box>
-            <Typography variant="h6" fontFamily="monospace">Menu</Typography>
-          </Box>
-          <Box>
-            <Typography variant="h4" fontFamily="monospace">Flash Trivia</Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" fontFamily="monospace">Account</Typography>
-          </Box>
-        </Stack>
-      </Box>
-      <Grid container component="main" alignContent="center" sx={{ height: '100vh', width: '100vw'}}>
-        <CssBaseline />
-        <Grid
-          item
+          <Typography variant="h2">Flash Trivia</Typography>
+          <Typography variant="h5">Insert cool info here</Typography>
+          <Button variant="filled" sx={{backgroundColor: 'orange', border: '2px solid DarkOrange', boxShadow: '0px 0px 5px DarkOrange'}}>Get Started</Button>
+        </Box>
+
+        <Box
+          width="100%"
+          display="flex"
+          flexDirection="column"
           alignItems="center"
-          justifyContent="center"
-          alignContent="center"
-          m="auto"
-          xs={false}
-          sm={9}
-          sx={{
-            backgroundSize: 'cover',
-            backgroundPosition: 'left',
-            backgroundColor: '#0F0F0F',
-          }}
+          padding={4}
         >
-          <Stack
-            direction="column" width="100%" alignContent="center" alignItems="center" spacing={4}
-          >
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <Typography variant="h3">Front</Typography>
-                  </div>
-                  <div class="flip-card-back">
-                    <Typography variant="h3">Back</Typography>
-                  </div>  
-                </div>
-            </div>
-            <Box borderRadius= "15px" p={2} height="15vh" width="40vw" backgroundColor="#040404" boxShadow="0px 0px 15px #4A4A4A" alignContent="center">
-              <Stack
-                direction="row" height="100%" justifyContent="space-between">
-                  <Stack
-                    direction="column" height="100%" width="80%"
-                  >
-                    <Typography fontFamily="monospace" variant="h6">Type answer below:</Typography>
-                    <Box borderRadius="15px" backgroundColor="#BFBFBF" height="52%" width="100%">
-                      <TextField fullWidth InputProps={{style : {borderRadius: "15px"}}}></TextField>
-                    </Box>
-                  </Stack>
-                
-                <Button variant="filled" sx={{backgroundColor: 'orange', border: '2px solid DarkOrange', boxShadow: '0px 0px 5px DarkOrange'}}>GO</Button>
-              </Stack>
-            </Box>
-          </Stack>
-        </Grid>
-        <Grid
-          item
-          xs={false} 
-          sm={3} 
-          md={3} 
-          height="95%"  
-          elevation={6} 
-          square 
-          borderRadius="15px" 
-          backgroundColor='#FFFFFF' 
-          boxShadow="0px 0px 20px #5F5F5F"
-        >
-          <Stack
-            direction="column"
-            fullWidth
-            height="100%"
-            p={2}
-            spacing={2}
-            justifyContent="space-between"
-          >
-            <Stack
-              width="100%"
-              height="90%"
-              direction="column"
-            >
-              <Box border="2px solid #040404" borderRadius="15px" fullWidth height="100%"/>
-            </Stack>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              alignItems="center"
-              spacing={2}
-            >
-              <TextField fullWidth sx={{border: '2px solid black', borderRadius: '15px'}} InputProps={{style : {borderRadius: "12px"}}}></TextField>
-              <Button variant="filled" sx={{backgroundColor: 'orange', border: '2px solid DarkOrange', boxShadow: '0px 0px 5px DarkOrange'}}>Send</Button>
-            </Stack>
-          </Stack>
-        </Grid>
-      </Grid>
-    </Stack>
+          <Typography variant="h4">One Payment</Typography>
+          <Typography variant="h6">We follow a one-time payment model to ensure that our service is accesible for all.</Typography>
+          <Button variant="filled" sx={{backgroundColor: 'orange', border: '2px solid DarkOrange', boxShadow: '0px 0px 5px DarkOrange'}}>Subscribe</Button>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
